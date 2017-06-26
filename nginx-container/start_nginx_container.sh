909d2d0a503835e2c38f57d56803c766763bea4e
jeff-jk <436167+jeff-jk@users.noreply.github.com>
2017-06-25 22:01:31 -0500

    Add shebang

--- nginx-container/start_nginx_container.sh
+++ nginx-container/start_nginx_container.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 docker run -d -p 80:80 -p 443:443 \
 -v /home/letsencrypt/nginx-docker-volume:/var/www/mindlessflowcom/htdocs \
 -v /home/letsencrypt/mindlessflowcom/mindlessflowcom_cert_chain.crt:/etc/ssl/nginx/mindlessflowcom_crt.pem \
