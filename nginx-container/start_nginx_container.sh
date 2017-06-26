1bfc82caa6320c7f9569ac07a18c8bd4f1d57a7a
jeff-jk <436167+jeff-jk@users.noreply.github.com>
2017-06-25 21:58:44 -0500

    Add start_nginx_container.sh

---
+++ nginx-container/start_nginx_container.sh
@@ -0,0 +1,6 @@
+docker run -d -p 80:80 -p 443:443 \
+-v /home/letsencrypt/nginx-docker-volume:/var/www/mindlessflowcom/htdocs \
+-v /home/letsencrypt/mindlessflowcom/mindlessflowcom_cert_chain.crt:/etc/ssl/nginx/mindlessflowcom_crt.pem \
+-v /home/letsencrypt/mindlessflowcom/letsencrypt_mindlessflowcom_domain.key:/etc/ssl/nginx/mindlessflowcom_key.pem \
+-v /home/letsencrypt/mindlessflowcom/mindlessflowcom_dhparams.pem:/etc/ssl/nginx/mindlessflowcom_dhparams.pem \
+registry.gitlab.com/team-vince-jeff/matrix-docker/mindless-nginx:latest
