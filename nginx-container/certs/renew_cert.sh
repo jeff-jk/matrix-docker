6d8af40fc2ddb24ccf538e07cb479a7ff40fdf8d
jeff-jk <436167+jeff-jk@users.noreply.github.com>
2017-10-01 15:05:31 -0400

    Fixed certs, moved files around, added database

---
+++ nginx-container/certs/renew_cert.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+python /path/to/acme_tiny.py --account-key /path/to/account.key --csr /path/to/domain.csr --acme-dir /var/www/challenges/ > /tmp/signed.crt || exit
+wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > intermediate.pem
+cat /tmp/signed.crt intermediate.pem > /path/to/chained.pem
+service nginx reload
