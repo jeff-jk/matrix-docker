76957c6a315bbc90b77e2ad46b4377b45828622b
jeff-jk <436167+jeff-jk@users.noreply.github.com>
2017-11-10 19:17:03 +0000

    Fixed the matrix-container and homeserver keys

--- start-matrix.sh
+++ start-matrix.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 8448:8448 -p 3478:3478 --expose 8048 --name=synapse --network=chat_nw -v /root/matrix-docker/volumes/data:/data registry.gitlab.com/team-vince-jeff/matrix-docker:beta start
+docker run -d -p 8448:8448 -p 3478:3478 --expose 8048 --name=synapse --network=chat_nw -v /root/working/matrix-docker/volumes/data:/data registry.gitlab.com/team-vince-jeff/matrix-docker/mindless-matrix:beta start
